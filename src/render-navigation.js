b9ea26b2f013e468048a44697aaa6bb153d764d7
gen <kyliki@yandex.ru>
2025-03-08 00:33:47 +0300

    render rating layout

--- src/render-navigation.js
+++ src/render-navigation.js
@@ -3,39 +3,25 @@ import { createChatControl } from './chat/create-chat-control.js';
 import { addSpriteToEnd } from './utils/add-sprite-to-end.js';
 import { createButton } from './utils/create-button.js';
 import { characterControl } from './university/character-control.js';
+import { createRating } from './rating/rating.js';
 
 export async function renderNavigation(app) {
   const navContainer = new Container();
-  const paddingBottom = 18;
-  const navHeight = 63;
-  navContainer.height = navHeight;
 
   const chatControl = createChatControl();
 
   addSpriteToEnd(navContainer, chatControl, 0);
 
-  function onClickButtonUniversity() {
-    console.log('clicked');
-  }
-
   const buttonUniversity = characterControl(app);
   addSpriteToEnd(navContainer, buttonUniversity, 8);
-
-  function onClickButtonPost() {
-    console.log('clicked');
-  }
 
   const buttonPost = createButton('buttonPost');
   addSpriteToEnd(navContainer, buttonPost, 8);
 
-  function onClickButtonRating() {
-    console.log('clicked');
-  }
-
-  const buttonRating = createButton('buttonRating');
+  const { buttonRating, ratingLayout } = createRating(app);
   addSpriteToEnd(navContainer, buttonRating, 8);
 
   navContainer.x = 10;
   navContainer.y = 549;
-  app.stage.addChild(navContainer);
+  app.stage.addChild(navContainer, ratingLayout);
 }
